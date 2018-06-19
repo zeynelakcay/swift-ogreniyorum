26d8e35aed9d8cf48e1a56fcfc9d8f2652e7d335
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-06-19 18:23:01 +0300

    Swift Arrays

---
+++ swift-diziler.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Swift’te dizi elemanları aynı türden olmalıdır. Heterojen yapıda olamaz.
+
+import UIKit
+
+var array : [String] = ["zeynel","akçay","bilgisayar mühendisi"]
+
+var array1 : [Int] =  [1,2,100]
+
+for item in array {
+    print(item)
+}
+for item in array1 {
+    print(item)
+}
+
+
+
+
+

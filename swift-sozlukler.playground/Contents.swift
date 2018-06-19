631a9489b2d043d2dade8d0f698bf86a8ebffcf3
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-06-19 20:43:50 +0300

    Swift Dictionaries

---
+++ swift-sozlukler.playground/Contents.swift
@@ -0,0 +1,13 @@
+//Sözlükler dizilerden farklı olarak, veri ikililerinden oluşur - [ Anahtar(Key) :  Veri (Data)]
+//Dizilerde olduğu gibi, sözlük elemanları da aynı türden olmalıdır ve heterojen yapıda olamaz
+
+import UIKit
+
+var sozlukler:Dictionary<Int,String> = [1:"zeynel",2:"akcay",3:"bilgisayar",4:"mühendisi"]
+
+for item in sozlukler
+{
+    print(item)
+}
+
+

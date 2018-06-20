3f12526ece7e56ac8897ce98fc006579c6670751
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-06-20 12:05:42 +0300

    Swift Functions

---
+++ swift-fonksiyonlar.playground/Contents.swift
@@ -0,0 +1,24 @@
+// SWİFT FONSKSİYON
+
+//func FonksiyonAdi(Parametre Listesi)->Tip
+
+//{ Lokal Değişkenler
+
+//    komutlar
+
+//    return değer;
+
+//}
+
+import UIKit
+
+
+func sum(par1:Int,par2:Int)->Int
+{
+    return par1 + par2;
+}
+
+var sum1  = sum(par1: 20, par2: 30)
+
+print(sum1)
+

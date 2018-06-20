981e341e1df5b7d3273bb665c982931a1114cb57
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-06-20 10:45:59 +0300

    Swift Control Statemnets

---
+++ swift-kontrol-komutlari.playground/Contents.swift
@@ -0,0 +1,41 @@
+//IF KOMUTU:
+
+//Genel Kullanım if koşul {TRUE} else {FALSE} şeklindedir
+
+//Koşul doğru (true) ise TRUE kısmı, koşul yanlış (false) ise de FALSE kısmı icra edilir.
+
+import UIKit
+
+var a:Int = 6 // Tür belirlemesi yapmadan da değişken tanımlayabiliriz, derleyici türünü algılayacaktır.
+var b = 8
+
+if a > b {
+    print("\(a) sayısı \(b)'den büyüktür")
+}else{
+    print("\(a) sayısı \(b)'den küçüktür")
+}
+
+
+// FOR KOMUTU
+
+var end = 10
+var sum  = 0
+var i:Int
+for i in 1...end {
+    sum = sum + i
+}
+print("Sum : \(sum)")
+
+
+//Switch/Case YAPISI
+
+
+
+//WHİLE DEYİMİ
+
+
+//DO WHİLE DEYİMİ
+
+
+
+
